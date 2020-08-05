0d7894a4184fbe7ac644ce7ffed39f29baac77e0
Andrew Molina <2997206+akmolina28@users.noreply.github.com>
2020-08-05 21:14:14 +0000

    Added support for OAuth authentication, allowing for calendar api access using a real google account rather than a service account

--- src/GoogleCalendarFactory.php
+++ src/GoogleCalendarFactory.php
@@ -20,16 +20,46 @@ class GoogleCalendarFactory
 
     public static function createAuthenticatedGoogleClient(array $config): Google_Client
     {
+        $authProfile = $config['default_auth_profile'];
+
+        switch($authProfile) {
+            case 'service_account':
+                return self::createServiceAccountClient($config['auth_profiles']['service_account']);
+            case 'oauth':
+                return self::createOAuthClient($config['auth_profiles']['oauth']);
+        }
+
+        throw new \InvalidArgumentException("Unsupported authentication profile [{$authProfile}].");
+    }
+
+    protected static function createServiceAccountClient(array $authProfile): Google_Client
+    {
         $client = new Google_Client;
 
         $client->setScopes([
             Google_Service_Calendar::CALENDAR,
         ]);
 
-        $client->setAuthConfig($config['service_account_credentials_json']);
+        $client->setAuthConfig($authProfile['credentials_json']);
 
         return $client;
     }
+
+    protected static function createOAuthClient(array $authProfile): Google_Client
+    {
+        $client = new Google_Client;
+
+        $client->setScopes([
+            Google_Service_Calendar::CALENDAR,
+        ]);
+
+        $client->setAuthConfig($authProfile['credentials_json']);
+
+        $client->setAccessToken(file_get_contents($authProfile['token_json']));
+
+        return $client;
+    }
+
 
     protected static function createCalendarClient(Google_Service_Calendar $service, string $calendarId): GoogleCalendar
     {
--- src/GoogleCalendarServiceProvider.php
+++ src/GoogleCalendarServiceProvider.php
@@ -35,14 +35,46 @@ class GoogleCalendarServiceProvider extends ServiceProvider
             throw InvalidConfiguration::calendarIdNotSpecified();
         }
 
-        $credentials = $config['service_account_credentials_json'];
+        $authProfile = $config['default_auth_profile'];
 
-        if (! is_array($credentials) && ! is_string($credentials)) {
-            throw InvalidConfiguration::credentialsTypeWrong($credentials);
+        switch ($authProfile) {
+            case 'service_account':
+                $this->validateServiceAccountConfigSettings($config);
+                break;
+            case 'oauth':
+                $this->validateOAuthConfigSettings($config);
+                break;
+            default:
+                throw new \InvalidArgumentException("Unsupported authentication profile [{$authProfile}].");
+        }
+    }
+
+    protected function validateServiceAccountConfigSettings(array $config = null)
+    {
+        $credentials = $config['auth_profiles']['service_account']['credentials_json'];
+
+        $this->validateConfigSetting($credentials);
+    }
+
+    protected function validateOAuthConfigSettings(array $config = null)
+    {
+        $credentials = $config['auth_profiles']['oauth']['credentials_json'];
+
+        $this->validateConfigSetting($credentials);
+
+        $token = $config['auth_profiles']['oauth']['token_json'];
+
+        $this->validateConfigSetting($token);
+    }
+
+    protected function validateConfigSetting(string $setting)
+    {
+        if (! is_array($setting) && ! is_string($setting)) {
+            throw InvalidConfiguration::credentialsTypeWrong($setting);
         }
 
-        if (is_string($credentials) && ! file_exists($credentials)) {
-            throw InvalidConfiguration::credentialsJsonDoesNotExist($credentials);
+        if (is_string($setting) && ! file_exists($setting)) {
+            throw InvalidConfiguration::credentialsJsonDoesNotExist($setting);
         }
     }
 }
