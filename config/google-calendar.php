6503fa85a14bf948dd9b08e3fc735e4f30897ce5
Rafael Mundel <rmundel@gmail.com>
2021-09-26 20:37:10 +0000

    Domain-wide delegation support

--- config/google-calendar.php
+++ config/google-calendar.php
@@ -36,4 +36,9 @@ return [
      *  The id of the Google Calendar that will be used by default.
      */
     'calendar_id' => env('GOOGLE_CALENDAR_ID'),
+
+     /*
+     *  The email address of the user account to impersonate.
+     */
+    'user_to_impersonate' => env('GOOGLE_CALENDAR_IMPERSONATE'),
 ];
